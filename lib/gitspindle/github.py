b4cf676b1d9410744c47dc75f2837a61bca10bc4
Abdo Roig-Maranges <abdo.roig@gmail.com>
2016-04-03 03:29:55 +0200

    github: adapt to changes in github3.py 1.0.0
    create_repo has been renamed to create_repository, and delete_key has
    been removed.

--- lib/gitspindle/github.py
+++ lib/gitspindle/github.py
@@ -538,7 +538,7 @@ class GitHub(GitSpindle):
 
         if name in [x.name for x in dest.repositories() if x.owner.login == ns]:
             err("Repository already exists")
-        repo = dest.create_repo(name=name, description=opts['--description'] or "", private=opts['--private'])
+        repo = dest.create_repository(name=name, description=opts['--description'] or "", private=opts['--private'])
 
         if 'origin' in self.remotes():
             print("Remote 'origin' already exists, adding the GitHub repository as 'github'")
@@ -1239,7 +1239,7 @@ class GitHub(GitSpindle):
            Remove deploy key by id"""
         repo = self.repository(opts)
         for key in opts['<key>']:
-            repo.delete_key(key)
+            repo.key(key).delete()
 
     @command
     def remove_hook(self, opts):
